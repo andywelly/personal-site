a898c917324b9f19300a0545aa84744661e56c73
Andwele Ancheta <andwele.ancheta@gmail.com>
2025-02-08 18:09:02 +1100

    Complete Basic Structure

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+// app/about/page.tsx
+export default function AboutPage() {
+    return (
+      <section>
+        <h1>About Me</h1>
+        <p>Here is some information about me.</p>
+      </section>
+    );
+  }
+  
