9aca36c3c090ad6177db0c50baf4211b5a54d37b
Andwele Ancheta <andwele.ancheta@gmail.com>
2025-02-09 22:54:31 +1100

    Initial Mui Test

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,12 @@
 // app/contact/page.tsx
+import Button from '@mui/material/Button';
+
 export default function ContactPage() {
     return (
       <section>
         <h1>Contact Me</h1>
         <p>Feel free to reach out through the following channels.</p>
+        <Button variant="contained">Hello world</Button>
       </section>
     );
   }
