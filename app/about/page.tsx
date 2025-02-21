994aac60439de9ca510ed15a9c74497549733911
Andwele Ancheta <andwele.ancheta@gmail.com>
2025-02-22 02:55:45 +1100

    Add Initial about timeline

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,11 @@
+import AboutTimeline from "@/components/abouttimeline";
+
 // app/about/page.tsx
 export default function AboutPage() {
     return (
       <section>
-        <h1>About Me</h1>
+        <h1 className="text-4xl font-bold mb-4 py-4 text-center">About Me</h1>
+        <AboutTimeline/>
       </section>
     );
   }
