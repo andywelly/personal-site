5696668946551cc0e77933ba6e06447240c92ec1
Andwele Ancheta <andwele.ancheta@gmail.com>
2025-02-23 02:40:49 +1100

    Add Project links to header

--- components/abouttimeline.tsx
+++ components/abouttimeline.tsx
@@ -12,6 +12,10 @@ import {
 } from '@mui/lab';
 import { School, Work, VolunteerActivism, CardMembership, Computer, SportsBasketball } from '@mui/icons-material';
 import Image from 'next/image';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+
 
 export default function AboutPage() {
   useEffect(() => {
@@ -43,7 +47,18 @@ export default function AboutPage() {
           {/* Left Column */}
           <div className="w-full md:w-1/2">
             <h1 className="text-5xl font-bold mb-4 text-left">About Me</h1>
-            <div className="mt-8">
+            <div className="social-links flex justify-left gap-4 relative">
+              <a href="https://github.com/andywelly" target="_blank" rel="noopener noreferrer">
+                <GitHubIcon fontSize="large" />
+              </a>
+              <a href="https://www.linkedin.com/in/andwele-ancheta/" target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon fontSize="large" />
+              </a>
+              <a href="mailto:andwele.ancheta@outlook.com">
+                <EmailIcon fontSize="large" />
+              </a>
+            </div>
+            <div className='mt-4'>
               <h2 className="text-3xl font-bold mb-4">My Journey</h2>
               <p className="text-xl">
                 A timeline of my academic, professional, and volunteer experiences.
