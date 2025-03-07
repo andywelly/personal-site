828515bea28f7353e38232ab83df7043ab213218
Andwele Ancheta <andwele.ancheta@gmail.com>
2025-03-08 04:05:39 +1100

    Add autocomplete card page

--- components/autocompletecard.tsx
+++ components/autocompletecard.tsx
@@ -6,16 +6,16 @@ export default function AutoCompleteCard() {
   return (
     <div className="text-[#e0e0e0] font-inter flex flex-col items-center justify-center">
       <div className="w-full mx-auto">
-        <h1 className="text-3xl font-bold text-center mb-8">Search AutoComplete Lookup</h1>
+        <h1 className="text-3xl font-bold text-center mb-8">Autocomplete Optimisation: A Comparative Analysis</h1>
 
         <div className="flex gap-8">
           {/* Left Column: Image */}
           <div className="w-1/2">
             <Image
-              src="/pacman.png"
-              alt="Pacman Game Demo"
-              width={600}
+              src="/dataset.png"
+              alt="Dataset Image"
               height={400}
+              width={600}
               className="w-full h-auto rounded-lg"
             />
           </div>
@@ -25,25 +25,17 @@ export default function AutoCompleteCard() {
             {/* First Section of Text */}
             <div className="mb-6">
               <p className="text-base text-center">
-            Developed a Pac-Man game in Java, implementing core mechanics such as character control, autonomous ghost AI, and score management.
-             This object-oriented software development project showcases proficiency in design principles and the software development life cycle  
+              This project explores the efficiency of autocomplete systems by comparing sorted arrays and radix trees, using a dataset of 
+              Melbourne restaurants. Through rigorous implementation and analysis, with findings detailed in a comprehensive report analysing 
+              insertion and retrieval speeds, I evaluated the performance of each structure.
               </p>
             </div>
             <div>
               <div className="flex flex-wrap gap-4 justify-center py-3"> 
-              <a href="https://www.java.com/en/" target="_blank" rel="noopener noreferrer" title="Java"> {/* Link for Java */}
+              <a href="https://www.w3schools.com/c/c_intro.php" target="_blank" rel="noopener noreferrer" title="Java"> {/* Link for Java */}
                 <Image
-                  src="/java.svg" 
-                  alt="Java Logo"
-                  width={50}
-                  height={50}
-                />
-              </a>
-
-              <a href="https://www.lwjgl.org/" target="_blank" rel="noopener noreferrer" title="LWJGL"> {/* Link for LWJGL */}
-                <Image
-                  src="/lwjgl.svg" 
-                  alt="LWJGL Logo"
+                  src="/c.png" 
+                  alt="C Logo"
                   width={50}
                   height={50}
                 />
@@ -51,7 +43,7 @@ export default function AutoCompleteCard() {
               </div>
               <div className='text-center mt-4'>
                   <Link
-                    href="/pacman"
+                    href="/autocomplete"
                   className="text-white hover:bg-[var(--pacman-color)] hover:border-4 border-dashed hover:border-white px-6 py-2 rounded transition-all duration-300"
                   >
                     Read More
